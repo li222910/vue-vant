c05505338a79984d7670ead29afcc3066308a2a3
test <3212173171@qq.com>
2021-06-04 10:24:04 +0800

    1

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+            <van-tabbar-item icon="search">标签</van-tabbar-item>
+            <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+            <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+<script>
+import {Tabbar, TabbarItem} from "vant"
+export default {
+    name:"home",
+    data() {
+        return {
+            active: 0,
+        }
+    },
+    component:{
+        Tabbar,
+        TabbarItem
+    },
+}
+</script>
